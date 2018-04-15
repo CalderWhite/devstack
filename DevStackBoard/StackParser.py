6f1e74bc9d27a3bcdb3534941ecb2dd097200925
Calder White <calderwhite1@gmail.com>
2018-04-15 00:39:37 +0000

    Improved stack parsing algorithm.

--- DevStackBoard/StackParser.py
+++ DevStackBoard/StackParser.py
@@ -6,7 +6,7 @@ class StackParser(object):
         self.DELIMITER_RULE = r'[\-\,]'
         # the largest distance between two delimiters (characterized by DELIMITER_RULE)
         # for it considered to indicated a list
-        self.MAX_DELIMITER_DISTANCE = 20
+        self.MAX_DELIMITER_DISTANCE = 3
         # the minimum amount of items in a dev stack list
         self.MIN_LIST_LENGTH = 3
         # a set of regex rules to filter out stacks that aren't actually stacks
@@ -14,11 +14,13 @@ class StackParser(object):
             r'([0-9]+|[0-9]+\,) employees [0-9]+ agents'
         ]
         # characters that will be removed from stack items
-        self.BAD_CHARACTERS = list(",(){}[]\n\t")
+        self.BAD_CHARACTERS = list("(){}[]\n\t")
         
         self.REPLACES = [
             (r'<br/>',"\n"),
-            (r'(and\/or|and|or|[\,\/])',",")
+            (r'( and\/or | and | or |[\,\/])',","),
+            (r'[,]+',", "),
+            (r'\\x[a-z0-9]{2}',"")
         ]
     def replace_terms(self,s):
         for i in self.REPLACES:
@@ -31,41 +33,37 @@ class StackParser(object):
         return s
 
     def find_stacks(self,text):
-        text = self.replace_terms(text)
+        text = self.cleanse_item(str(text.encode('ascii','ignore')))
+        text = self.replace_terms(text).split(" ")
         
-        prev = -self.MAX_DELIMITER_DISTANCE - 1
         prevAdded = False
         commas = []
-        for match in re.finditer(self.DELIMITER_RULE,text):
-            # we only care about the first, since we're only matching one character
-            current_index = match.span()[0]
-            
-            # if they are close together, act on them
-            if current_index - prev < self.MAX_DELIMITER_DISTANCE:
+        
+        indicies = []
+        
+        for w in range(len(text)):
+            if text[w].find(",") > -1:
+                indicies.append(w)
+                text[w] = text[w].replace(",","")
                 
-                if not prevAdded:
+        
+        prev = -self.MAX_DELIMITER_DISTANCE
+        prev_added = False
+        
+        for i in indicies:
+            if i - prev < self.MAX_DELIMITER_DISTANCE:
+                if not prev_added:
                     commas.append([])
-                    prev_index = prev
-                    s = ""
-                    while prev_index > 0 and text[prev_index] != " ":
-                        s = text[prev_index] + s
-                        prev_index -= 1
-                    commas[-1].append(
-                        self.cleanse_item(s)
-                    )
-                # if we aren't at the end of the string, to avoid an IndexError
-                current_index -= 1
-                s = ""
-                while current_index > 0 and text[current_index] != " ":
-                    s = text[current_index] + s
-                    current_index -= 1
-                commas[-1].append(
-                    self.cleanse_item(s)
-                )
-                prevAdded = True
+                    # the first item of a list shouldn't be added, since it is still far away from other commas.
+                    # but once we figure out that the string of comma delimited words _is_ a list, 
+                    # we add the first item.
+                    commas[-1].append(text[indicies[indicies.index(i)-1]])
+                commas[-1].append(text[i])
+                prev_added = True
             else:
-                prevAdded = False
-            prev = current_index
+                prev_added = False
+            prev = i
+        
         for i in range(len(commas)-1,-1,-1):
             if len(commas[i]) < self.MIN_LIST_LENGTH:
                 commas.pop(i)
@@ -78,22 +76,7 @@ class StackParser(object):
         return commas
 
 def main():
-    r = open('dist.json')
-    j = json.loads(r.read())
     s = StackParser()
-    stacks = []
-    for company in j:
-        for job in j[company]:
-            new_stacks = s.find_stacks(job["description"])
-            stacks.append(
-                new_stacks
-            )
-    stacks.sort(key=lambda key: len(key),reverse=True)
-    for i,stack in enumerate(stacks):
-        print(",".join(stack))
-    #print(len(comps))
-    with open("stacks.json",'w') as w:
-        w.write(json.dumps(stacks,indent=4))
-
+    x = s.find_stacks("-Develop and SCALE our website as well as internal systems using Perl, DBI, Mason, Sereal, Plack, JavaScript, MySQL, Memcached, Redis, Hadoop, Elasticsearch, git, and CentOS Linux. <br/>-We are agile and work in small scrum teams so expect innovation and experiments!")
 if __name__ == '__main__':
     main()
